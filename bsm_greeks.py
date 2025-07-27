c3de279fcf0f547c1f1dc71854898710b7791d76
JMGurgeh <hpenros@yahoo.co.uk>
2025-07-27 20:23:54 +0100

    Updated the bsm model to allow prices to be treated as arrays
    Fixed the bsm model treating the call and put
    outputs as variables, instead wrapping them into
    functions.
    
    Updated the Greeks model to plot the error between
    analytical and numerical differentiation methods
    for delta.

--- bsm_greeks.py
+++ bsm_greeks.py
@@ -1,6 +1,6 @@
 # calculating the 'Greeks' 
 
-import bsm_model
+import bsm_model # importing the model we built in bsm model
 import math
 import numpy as np
 from scipy.stats import norm
@@ -12,16 +12,57 @@ T = 0.8     # time to maturity in years
 r = 0.05    # annualised risk-free rate
 sigma = 0.2 # realised volatility 
 q = 0.0     # the dividend yield 
-ds = 1e-5
+ds = 1e-5   # the increment in stock price that we use for differentiation
+
+def d1(S, K, T, r, sigma):
+    return (np.log(S/K) + (r + sigma**2/2)*T) /\
+                     (sigma*np.sqrt(T))
+
+def d2(S, K, T, r, sigma):
+    return d1(S, K, T, r, sigma) - sigma* np.sqrt(T)
+
+def delta_call(S, K, T, r, sigma):
+    N = norm.cdf
+    return N(d1(S, K, T, r, sigma))
+
+def delta_put(S, K, T, r, sigma):
+    N = norm.cdf
+    return -N(-d1(S, K, T, r, sigma))
 
 def delta_fdm_call(S, K, T, r, sigma, q, ds):
-    return (bsm_model.our_call(S + ds, K, T, r, sigma, q) - bsm_model.our_call(S, K, T, r, sigma, q))/ds
+    return (bsm_model.our_call(S + ds, K, T, r, sigma, q) - 
+            bsm_model.our_call(S, K, T, r, sigma, q))/ds
 
 def delta_fdm_put(S, K, T, r, sigma, q, ds):
-    return (bsm_model.our_put(S + ds, K, T, r, sigma, q) - bsm_model.our_put(S, K, T, r, sigma, q))/ds
+    return (bsm_model.our_put(S + ds, K, T, r, sigma, q) - 
+            bsm_model.our_put(S, K, T, r, sigma, q))/ds
 
 # using arange to generate a set of prices against which we can plot the delta
 prices = np.arange(1, 300, 1)
-deltas_call = delta_fdm_call(prices, K, T, r, sigma, q)
-deltas_put = delta_fdm_put(prices, K, T, r, sigma, q)
+deltas_call = delta_call(prices, K, T, r, sigma)
+deltas_put = delta_put(prices, K, T, r, sigma)
+deltas_fdm_call = delta_fdm_call(prices, K, T,r, sigma, q, ds)
+deltas_fdm_put = delta_fdm_put(prices, K, T,r, sigma, q, ds)
 
+plt.style.use("seaborn-v0_8-darkgrid")
+plt.figure(1)
+plt.plot(prices, deltas_call, label = 'Call Delta')
+plt.plot(prices, deltas_put, label = 'Put Delta')
+plt.xlabel('$S_0$')
+plt.ylabel('Delta')
+plt.title('Effect of underlying stock price on option delta')
+plt.axvline(K, color='black', linestyle='dashed', linewidth=2,label="Strike")
+plt.legend()
+plt.show(block = False)
+
+# looking at the error between the analytical and numerical methods
+call_error = np.array(deltas_call) - np.array(deltas_fdm_call)
+put_error = np.array(deltas_put) - np.array(deltas_fdm_put)
+
+plt.figure(2)
+plt.plot(prices, call_error, label = 'Call method errors')
+plt.plot(prices, put_error, label = 'Put method errors')
+plt.legend()
+plt.xlabel('$S_0$')
+plt.ylabel('FDM Error')
+plt.show()
--- bsm_model.py
+++ bsm_model.py
@@ -9,13 +9,13 @@ import blackscholes as bsm_ref # reference function to check our model
 # writing out the main BSM function
 
 def bsm(S, K, T, r, sigma, q = 0, op_type = 'call'):
-    d1 = (math.log(S/K) + T * (r + (sigma**2)/2))/(sigma * math.sqrt(T))
-    d2 = d1 - (sigma * math.sqrt(T))
+    d1 = (np.log(S/K) + T * (r + (sigma**2)/2))/(sigma * np.sqrt(T))
+    d2 = d1 - (sigma * np.sqrt(T))
     if op_type == 'call':
-        call_value = S * math.exp(-q * T) * norm.cdf(d1, 0, 1) - K * math.exp(-r * T) * norm.cdf(d2, 0, 1)
+        call_value = S * np.exp(-q * T) * norm.cdf(d1, 0, 1) - K * np.exp(-r * T) * norm.cdf(d2, 0, 1)
         return call_value
     else:
-        put_value = K * math.exp(-r * T) * norm.cdf(-d2, 0 , 1) - S * math.exp(-q * T) * norm.cdf(-d1, 0, 1)
+        put_value = K * np.exp(-r * T) * norm.cdf(-d2, 0 , 1) - S * np.exp(-q * T) * norm.cdf(-d1, 0, 1)
         return put_value
 
 S = 100.0   # price of the underlying
@@ -31,10 +31,8 @@ ref_call_price = ref_call.price()
 ref_put = bsm_ref.BlackScholesPut(S, K, T, r, sigma, q)
 ref_put_price = ref_put.price()
 
-our_call = bsm(S, K, T, r, sigma, q, op_type= 'call')
-our_put = bsm(S, K, T, r, sigma, q, op_type= 'put')
+def our_call(S, K, T, r, sigma, q):
+    return bsm(S, K, T, r, sigma, q, op_type= 'call')
 
-print("Our Call, Reference Call")
-print(our_call, ref_call_price)
-print("Our Put, Reference Put")
-print(our_put, ref_put_price)
+def our_put(S, K, T, r, sigma, q):
+    return bsm(S, K, T, r, sigma, q, op_type= 'put')
